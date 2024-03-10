18e4e79e4b742474246e62abebe7eaacd02868d1
Zach Happel <zkhappel@gmail.com>
2024-03-10 18:55:28 -0400

    Building out algo

--- src/Parse.java
+++ src/Parse.java
@@ -4,32 +4,48 @@ import java.util.ArrayList;
 
 public class Parse {
 
+    // TODO: Add error handling? How to decide when it failed?
+
     //ArrayList<Token> token_stream;
     public static ArrayList<Production> derivation = new ArrayList<>();
     public static ArrayList<Token> token_stream;
     public static Toolkit toolkit;
+    public int token_pointer = 0;
 
     public Parse () { }
 
-    public void match (String seq) {
-
+    public Terminal match (String seq, int token_pointer_local) {
+        Terminal t = new Terminal(seq);
+        if (token_stream.get(token_pointer_local).getName() == seq) {
+            t.setSuccess(true);
+        }
+        return new Terminal("");
     }
 
     public void parseProgram () {
         Production program = new Production("Program");
-        parseBlock();
-        match("EOP");
+        parseBlock(program);
+        Terminal eop = match("EOP", token_pointer);
+        
+        if (eop.getSuccess()) {
+            derivation.add(program);
+            derivation.add(eop);
+        }
     }
 
-    public void parseBlock() {
+    public void parseBlock(Production program) {
+        
         Production block = new Production("Block");
-        match("KEYWORD_OPENBLOCK");
-        parseStatementList();
-        match("KEYWORD_CLOSEBLOCK");
+        Terminal open_block = match("KEYWORD_OPENBLOCK", token_pointer);
+        Production statement_list = parseStatementList();
+        match("KEYWORD_CLOSEBLOCK", token_pointer);
+
     }
 
-    public void parseStatementList() {
+    public Production parseStatementList() {
 
+
+        return new Production("STATEMENT_LIST")
     }
 
 
--- src/Production.java
+++ src/Production.java
@@ -1,9 +1,15 @@
 public class Production {
     
     public String name;
+    public Boolean success;
     
     public Production (String name) {
         this.name = name; 
     }
 
+    public String getName () {return this.name;}
+    public Boolean getSuccess () {return this.success;}
+
+    public void setSuccess (Boolean success) {this.success = success;}
+    
 }
