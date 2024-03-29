3fbdb893c3240763507e01f077332b3a81e3e02c
Zach Happel <zkhappel@gmail.com>
2024-03-29 05:31:22 -0400

    Messy messy messy. Decided to add AST tracking children and parents, just now. This will solve this issue theoretical but very real issue of trying to add and remove children mid recursion. This will allow me to track parent within the CST, adding a parent where appropriate to the AST, and then being able to navigate down the CST and children to the new parent within the AST. Basically, you keep a pointer to location within the CST and add/remove where you see fit within the AST... With rules set up for what gets added to the AST, which are encountered throughout the recursion, the final result will be the cut out parts that we are looking for. Recursion will still be done on the CST, but ..... I know what I am getting at. Going to work on this periodically and see how things come along. Fun fact, apparently using too much power on my PC caused the breaker in my apartment to flip earlier lol.. Needed to turn off the space heater to ensure that doesn't happen again.

--- src/Production.java
+++ src/Production.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 
-public class Production {
+public class Production implements Cloneable {
     
     public String name;
     public Boolean success = false;
     public ArrayList<Production> children = new ArrayList<Production>();
     public Production parent;
     public String prodKind;
+    
+    public ArrayList<Production> ast_children = new ArrayList<Production>();
+    public Production ast_parent;
 
     
     public Production (String name) {
@@ -18,20 +21,34 @@ public class Production {
         this.prodKind = prodKind;
     }
 
+    /*
+    public static Production copy( Production original ) {
+        Production new_prod = new Production(original.getName());
+        new_prod.prodKind = original.getProdKind();
+        new_prod.set = original.id;
+        //... etc. 
+        return newUser;
+   } */
+
     public String getName () {return this.name;}
     public Boolean success () {return this.success;}
     public ArrayList<Production> getChildren () {return this.children;}
-    public Production getParent () {return this.getParent();}
+    public Production getParent () {return this.parent;}
     public Production getChild(int index) {return children.get(index);}
     public String getProdKind() { return this.prodKind; }
-
     public void setName(String name) { this.name = name;}
     public void setSuccess (Boolean success) {this.success = success;}
-
     public void addChild (Production child) { this.children.add(child); }
     public void addParent (Production parent) { this.parent = parent;}
-
     public void removeChild (int index) { this.children.remove(index);}
+    public void removeChildren () {this.children = new ArrayList<>();}
+    
+    public Production getASTParent () {return this.ast_parent;}
+    public Production getASTChild(int index) {return ast_children.get(index);}
+    public void addASTChild (Production child) { this.ast_children.add(child); }
+    public void addASTParent (Production parent) { this.ast_parent = parent;}
+    public void removeASTChild (int index) { this.ast_children.remove(index);}
+    public void removeASTChildren () {this.ast_children = new ArrayList<>();}
 
 
 }
--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -1,15 +1,33 @@
 // Creating an AST from the CST
 
-
+/**
+ *       add("StatementList");
+        add("Statement");
+        add("Expression");
+        add("VarDeclStatement");
+        add("PrintStatement");
+        add("AssignmentStatement");
+        add("WhileStatement");
+        add("IfStatement");
+        add("BooleanExpression");
+        add("StringExpression");
+        add("CharacterList");
+ */
 import java.util.ArrayList;
 
 public class SemanticAnalysis {
 
 
+    ArrayList<Production> AST = new ArrayList<Production>();
+    Production current_parent;
+
     /**
      * There is a Character NonTerminal and there is a CHARACTER Terminal. The NonTerminal, Character, is always the parent to the Terminal, CHARACTER. Within the CHARACTER, is the value. 
      * 
      */
+
+
+    
 
     public String stringOfCharacters(int amount, String character) { String s = ""; for (int j = 0; j <= amount-1; j++) { s = s + character; } return s; }
 
@@ -25,35 +43,47 @@ public class SemanticAnalysis {
     }};
 
     public ArrayList<String> invalid_terminals = new ArrayList<String>(){{
-        add("KEYWORD_PRINT");
+        //add("KEYWORD_PRINT");
+        add("SYMBOL_OPENBLOCK");
+        add("SYMBOL_CLOSEBLOCK");
         add("SYMBOL_OPENPAREN");
         add("SYMBOL_CLOSEPAREN");
-        add("SYMBOL_STRINGEXPRBOUNDARY");
         add("SYMBOL_STRINGEXPRBOUNDARY");
         
     }};
 
+    
+    // No
     public ArrayList<String> valid_nonterminals = new ArrayList<String>(){{
         add("SYMBOL_EQUIVALENCE");
-        add("CHARACTER");
-        add("DIGIT");
         add("IDENTIFIER");
-        add("KEYWORD_TRUE");
-        add("KEYWORD_FALSE");
+        add("BLOCK");
     }};
 
+    public ArrayList<String> invalid_nonterminals = new ArrayList<String>(){{
+        add("StatementList");
+        add("Statement");
+        add("BLOCK");
+    }};
     
     public ArrayList<String> ACTIONABLE_NONTERMINALS = new ArrayList<String>(){{
         add("VarDeclStatement");
         
         add("PrintStatement"); // Summation of CharacterList within the Expression which is contained within print(" EXPR ")
-        // Ignored Terminals: print, (, ", )
+        // Ignored Terminals: KEYWORD_PRINT, SYMBOL_OPENPAREN, SYMBOL_STRINGEXPRBOUNDARY, SYMBOL_CLOSEPAREN
+        // PrintStatement is Parent, a nonterminal
+        // Analyzed further is Expression
+        // Think PrintStatement handling should reach down to Expression and then down to StringExpression because we know it is there
+        // -> This would mean we would never have to handle StringExpression again
+        // This would also deal with CharacterList
+        // PrintStatement parent will be new sort of terminal 
+
 
         add("AssignmentStatement");
         add("WhileStatement");
-        add("IfStatement");
+        add("IfStatement"); 
         add("BooleanExpression");
-        add("StringExpression");
+        //add("StringExpression");
         add("CharacterList");
     }};
 
@@ -68,11 +98,18 @@ public class SemanticAnalysis {
         add("AssignmentStatement");
         add("WhileStatement");
         add("IfStatement");
-        add("BooleanExpression");
+        
+        //add("BooleanExpression"); 
+        // Get its own weird thing
+
         add("StringExpression");
         add("CharacterList");
     }};
 
+    
+
+    
+    
     public void recursiveDescent (Production p, int index) {
 
         // These terminals all are child-most and their values are what is added to the AST, symbol table, etc. 
@@ -89,9 +126,82 @@ public class SemanticAnalysis {
             Production c = p.getChild(i);
             String spaces = stringOfCharacters(index * 2, " ");
             Boolean is_terminal = (c.getClass().getSimpleName()).equals("Terminal");
-            
-            
-            if (!is_terminal) { 
+
+            if (is_terminal) {
+                TerminalsList.add(c.getName());
+                Terminal terminal = (Terminal) c; 
+                
+                if (valid_terminals.contains(terminal.getName())) {
+                    current_parent.addChild(terminal);
+                    // could cause issues for top scope... being that the terminal would already be there 
+                }
+
+                //System.out.println("Terminal: " + c.getName()); 
+            } else {
+                NonTerminal nonterminal = (NonTerminal) c;
+                String nonterminal_name = nonterminal.getName();
+                
+                if ( nonterminal_name.equals("WhileStatment")) {
+
+                }
+
+                NonTerminalsList.add((NonTerminal) c);
+
+                //if (nonter)
+                
+                
+                //System.out.println("NonTerminal: " + c.getName());
+            }
+
+            recursiveDescent(c, index + 1);
+        }
+    }
+
+    public String getChildrenNames (NonTerminal nt) {
+        String children_names = "[";
+        for (int x = 0; x <= nt.getChildren().size() - 1; x++ ) {
+            children_names = children_names +  nt.getChild(x).getName() + "," ;
+        }
+        return children_names + "]";
+    }
+
+    
+    public void performSemanticAnalysis (ArrayList<Production> derivation, Toolkit tk ) {
+        System.out.println("\n\nSEMANTIC ANALYSIS:");
+        Production topscope_block = derivation.get(0).getChild(0); 
+        //Production topscope_copy = topscope_block.clone()
+        //AST.add(derivation.get(0).getChild(0)); // Block
+        //current_parent = derivation.get(0).getChild(0);
+        NonTerminal ast_starting_block = new NonTerminal("Block");
+        AST.add(ast_starting_block);
+        current_parent = ast_starting_block;
+        recursiveDescent(derivation.get(0).getChild(0), 1);
+        
+
+        for (int x = 0; x <= TerminalsList.size() - 1; x++) {
+            System.out.println("Terminal: " + TerminalsList.get(x));
+        }
+
+        System.out.println("\n\n");
+
+        for (int y = 0; y <= NonTerminalsList.size() - 1; y++) {
+            NonTerminal nonterminal = NonTerminalsList.get(y);
+            System.out.print("NonTerminal: " + nonterminal.getName());
+            System.out.print(nonterminal.getChildren().size() > 0 ? "         Children: " + getChildrenNames(nonterminal) + "\n": "0\n");
+
+        }    
+
+    }
+
+
+
+
+    
+}
+
+
+/**
+ *  if (!is_terminal) { 
                 String nonterminal_name = c.getName(); 
                 if ( !(nonterminal_name.contains("List")) ) {
                     System.out.println(spaces + index + stringOfCharacters(2, " ") + "   [" + c.getName() + "] "); 
@@ -119,46 +229,4 @@ public class SemanticAnalysis {
                     System.out.println("SKIPPING: " + terminal.getName()); 
                 }
             } 
-
-            if (is_terminal) {
-                //System.out.println("Terminal: " + c.getName()); 
-                TerminalsList.add(c.getName());
-            } else {
-                //System.out.println("NonTerminal: " + c.getName());
-                NonTerminalsList.add((NonTerminal) c); 
-            }
-
-            recursiveDescent(c, index + 1);
-        }
-    }
-
-    public String getChildrenNames (NonTerminal nt) {
-        String children_names = "[";
-        for (int x = 0; x <= nt.getChildren().size() - 1; x++ ) {
-            children_names = children_names +  nt.getChild(x).getName() + "," ;
-        }
-        return children_names + "]";
-    }
-
-    public void performSemanticAnalysis (ArrayList<Production> derivation, Toolkit tk ) {
-        System.out.println("\n\nSEMANTIC ANALYSIS:");
-        recursiveDescent(derivation.get(0), 0);
-
-        for (int x = 0; x <= TerminalsList.size() - 1; x++) {
-            System.out.println("Terminal: " + TerminalsList.get(x));
-        }
-        System.out.println("\n\n");
-        for (int y = 0; y <= NonTerminalsList.size() - 1; y++) {
-            NonTerminal nonterminal = NonTerminalsList.get(y);
-            System.out.print("NonTerminal: " + nonterminal.getName());
-            System.out.print(nonterminal.getChildren().size() > 0 ? "         Children: " + getChildrenNames(nonterminal) + "\n": "0\n");
-
-        }    
-
-    }
-
-
-
-
-    
-}
+ */
